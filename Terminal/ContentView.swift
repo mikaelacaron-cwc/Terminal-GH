956186351a6c8e07a6fe271e5a15ff8802504ae8
Mikaela Caron <mikaelacaron@gmail.com>
2021-01-10 13:22:12 -0500

    Initial Commit

---
+++ Terminal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Terminal
+//
+//  Created by Mikaela Caron on 1/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Terminal/TerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalApp.swift
+//  Terminal
+//
+//  Created by Mikaela Caron on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
